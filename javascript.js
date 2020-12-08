75c70590ebbc5f0a7fcec127b1ed1cb14fad7d69
viki179 <71555821+viki179@users.noreply.github.com>
2020-12-08 13:52:15 +0700

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,8 @@
+var n = prompt("Masukan Nama Anda:");
+var c = confirm("Hai " + n + " Selamat Datang di TOKO HP PIKI");
+var c = confirm(n + " Mau lihat-lihat Barang Kami ?");
+if (c == true) {
+    alert('Selamat Berbelanja');
+} else {
+    alert('No Problem');
+}
